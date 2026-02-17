38bd6d53d3bfbec3a2d7652cd974a00f596ebafb
rafaelzmdev <rafaelzmdev@gmail.com>
2026-02-17 17:47:24 -0300

    I'll have to re-do the wiring
    the buttons are completely wrong lol everything is shorting but I will fix it, will be cleaner than the mess it looks like right now

--- elevator1/elevator1.ino
+++ elevator1/elevator1.ino
@@ -1,21 +1,27 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
 }
 
 void loop() {
-  int blue = !digitalRead(2);
-  int red = !digitalRead(3);
-  delay(10);
-  if (red != blue) {
-    if (blue = 1) {
+  const int up = !digitalRead(3);
+  const int down = !digitalRead(2);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  if (up == 1 && down == 1) {
+    void;
+  }
+  else {
+    if (up == 1) {
       digitalWrite(4, HIGH);
+      Serial.println("4 is high");
     }
-    else if (red = 1) {
+    else if (down == 1) {
       digitalWrite(5, HIGH);
-    };
+      Serial.println("5 is high");
+    }
   };
 }
